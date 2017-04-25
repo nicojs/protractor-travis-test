1c9413ef8fe037fcdb1688cb9c528fcf5ce263a2
Nico Jansen <jansennico@gmail.com>
2017-04-25 20:25:04 +0200

    Configure selenium in travis startup

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,32 +1,32 @@
 const spawn = require('child_process').spawn;
 const os = require('os');
 
-function spawnWebdriverManager() {
-  return new Promise((res, rej) => {
-    const cmd = os.platform() === 'win32' ? 'webdriver-manager.cmd' : 'webdriver-manager';
-    const webdriverProcess = spawn(cmd, ['start']);
-    webdriverProcess.stdout.on('data', (data) => {
-      console.log(`stdout: ${data.toString()}`);
-    });
+// function spawnWebdriverManager() {
+//   return new Promise((res, rej) => {
+//     const cmd = os.platform() === 'win32' ? 'webdriver-manager.cmd' : 'webdriver-manager';
+//     const webdriverProcess = spawn(cmd, ['start']);
+//     webdriverProcess.stdout.on('data', (data) => {
+//       console.log(`stdout: ${data.toString()}`);
+//     });
 
-    webdriverProcess.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-      if (data.toString().indexOf('Selenium Server is up and running')) {
-        res();
-      }
-    });
+//     webdriverProcess.stderr.on('data', (data) => {
+//       console.log(`stderr: ${data}`);
+//       if (data.toString().indexOf('Selenium Server is up and running')) {
+//         res();
+//       }
+//     });
 
-    webdriverProcess.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-    });
+//     webdriverProcess.on('close', (code) => {
+//       console.log(`child process exited with code ${code}`);
+//     });
 
 
-    process.on('SIGINT', () => {
-      console.log('Received SIGINT, killing webdriver-manager');
-      webdriverProcess.kill('SIGINT');
-    });
-  });
-}
+//     process.on('SIGINT', () => {
+//       console.log('Received SIGINT, killing webdriver-manager');
+//       webdriverProcess.kill('SIGINT');
+//     });
+//   });
+// }
 
 var config = {
   directConnect: true,
@@ -44,17 +44,17 @@ var config = {
   }
 };
 
-// if (process.env.TRAVIS) {
-//   const webdriverPromise = spawnWebdriverManager();
+if (process.env.TRAVIS) {
+  // const webdriverPromise = spawnWebdriverManager();
 
-//   config.capabilities = {
-//     browserName: 'firefox'
-//   };
-//   config.seleniumAddress = 'http://localhost:4444/wd/hub';
-//   config.directConnect = false;
-//   config.beforeLaunch = function () {
-//     return webdriverPromise;
-//   }
-// }
+  // config.capabilities = {
+  //   browserName: 'firefox'
+  // };
+  config.seleniumAddress = 'http://localhost:4444/wd/hub';
+  config.directConnect = false;
+  config.beforeLaunch = function () {
+    return webdriverPromise;
+  }
+}
 
 exports.config = config;
